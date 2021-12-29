882a445d642abe7221350b243818da79f36a7a63
Steven Solomon <solomon.steven.m@gmail.com>
2021-12-29 11:22:19 -0500

    Add Java project

---
+++ src/main/java/com/javastatus/website/JavaStatusSiteApplication.java
@@ -0,0 +1,13 @@
+package com.javastatus.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStatusSiteApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaStatusSiteApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/javastatus/website/JavaStatusSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javastatus.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStatusSiteApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
